b2137c5c1b546d48ff7592bcc03929e07344ff53
Robert Tate <robertjosephtate@gmail.com>
2025-05-31 11:31:17 -0500

    add causality trap shorthand content menu item

--- src/functions/hooks/useCausalityTokenSetterMenuContext.ts
+++ src/functions/hooks/useCausalityTokenSetterMenuContext.ts
@@ -1,8 +1,11 @@
 import OBR, { isImage } from "@owlbear-rodeo/sdk";
 import { useEffect } from "react";
+import ShortUniqueId from "short-unique-id";
 
 import { ID } from "../../constants";
 import type { CausalityToken } from "../../types";
+
+const { randomUUID } = new ShortUniqueId({ length: 8 });
 
 export const useCausalityTokenSetterMenuContext = () => {
   useEffect(() => {
@@ -50,6 +53,75 @@ export const useCausalityTokenSetterMenuContext = () => {
                     causalities: [],
                   };
                 }
+              }
+            });
+            await OBR.player.deselect();
+            await OBR.action.open();
+          }
+        },
+      });
+
+      OBR.contextMenu.create({
+        id: `${ID}/causality-trap-menu`,
+        icons: [
+          {
+            icon: "/icon.svg",
+            label: "Add Causality Trap",
+            filter: {
+              every: [
+                { key: "type", value: "IMAGE" },
+                {
+                  key: [
+                    "metadata",
+                    "com.roberttate.causality",
+                    "isCausalityToken",
+                  ],
+                  value: false,
+                },
+              ],
+            },
+          },
+        ],
+        onClick: async (context) => {
+          if (isImage(context?.items?.[0])) {
+            await OBR.scene.items.updateItems(context?.items, (images) => {
+              for (let image of images) {
+                const selectedItem = image as CausalityToken;
+                if (selectedItem.visible === true) {
+                  selectedItem.visible = false;
+                }
+                const uniqueKey = randomUUID();
+                selectedItem.metadata[ID] = {
+                  isCausalityToken: true,
+                  causalities: [
+                    {
+                      id: uniqueKey,
+                      timestamp: new Date().toISOString(),
+                      cause: {
+                        status: "Pending",
+                        delay: "0",
+                        isCollided: false,
+                        tokenId: selectedItem.id,
+                        causalityId: uniqueKey,
+                        name: selectedItem.name,
+                        label: selectedItem.text?.plainText,
+                        imageUrl: selectedItem.image.url,
+                        trigger: "collision",
+                      },
+                      effects: [
+                        {
+                          tokenId: selectedItem.id,
+                          causalityId: uniqueKey,
+                          name: selectedItem.name,
+                          label: selectedItem.text?.plainText,
+                          imageUrl: selectedItem.image.url,
+                          effectId: randomUUID(),
+                          action: "appear",
+                        },
+                      ],
+                    },
+                  ],
+                };
               }
             });
             await OBR.player.deselect();
